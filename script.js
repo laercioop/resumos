68ce953592a497c5bb02976793c4e0bf553b2b27
Laercio Pachelli <Laercio@acpastore.com.br>
2026-07-07 15:09:46 -0300

    Exagera comicamente a animacao de hover do titulo Resumos
    Aumenta a inclinacao (ate 90 graus), o zoom e o efeito de profundidade,
    e deixa a transicao mais elastica.

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const rect = title.getBoundingClientRect();
     const x = (event.clientX - rect.left) / rect.width;
     const y = (event.clientY - rect.top) / rect.height;
-    const rotateY = (x - 0.5) * 34;
-    const rotateX = (0.5 - y) * 34;
-    inner.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+    const rotateY = (x - 0.5) * 90;
+    const rotateX = (0.5 - y) * 90;
+    inner.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.35) translateZ(50px)`;
   });
 
   title.addEventListener("mouseleave", () => {
-    inner.style.transform = "rotateX(0deg) rotateY(0deg) scale(1)";
+    inner.style.transform = "rotateX(0deg) rotateY(0deg) scale(1) translateZ(0px)";
   });
 });
